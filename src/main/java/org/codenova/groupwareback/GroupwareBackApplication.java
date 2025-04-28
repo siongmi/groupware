f95a458868bf212eb42dffc030581f8be0d081dc
siongmi <tldmseeo@naver.com>
2025-04-28 11:13:33 +0900

    시작

---
+++ src/main/java/org/codenova/groupwareback/GroupwareBackApplication.java
@@ -0,0 +1,13 @@
+package org.codenova.groupwareback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupwareBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupwareBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/codenova/groupwareback/GroupwareBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codenova.groupwareback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupwareBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
